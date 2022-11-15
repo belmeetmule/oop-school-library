9a7f2ec2d93f0340807d360e61a6ff5093d16200
mule <belmeetmule@gmail.com>
2022-11-15 13:58:41 +0300

    fix inheritance issue

--- teacher.rb
+++ teacher.rb
@@ -1,6 +1,6 @@
 require "./person"
 
-class Teacher
+class Teacher < Person
   def initialize(specialization, age, name = "unknown", parent_permission = true)
     super(age, name, parent_permission)
     @specialization = specialization
