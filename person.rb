87916cf60764c95c6717ca2edf2a9a1d331614e8
mule <belmeetmule@gmail.com>
2022-11-15 14:11:36 +0300

    fix linter errors

--- person.rb
+++ person.rb
@@ -1,24 +1,19 @@
 class Person
-  def initialize(age, name = "unknown", parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @age = age
     @name = name
     @parent_permission = parent_permission
   end
   attr_reader :id
-  attr_accessor :name
-  attr_accessor :age
+  attr_accessor :name, :age
 
-  def is_of_age?
-    if @age >= 18
-      true
-    else
-      false
-    end
+  def of_age?
+    @age >= 18
   end
 
   def can_use_services?
-    if is_of_age? || @parent_permission
+    if of_age? || @parent_permission
       true
     else
       false
@@ -26,7 +21,7 @@ class Person
   end
 end
 
-person = Person.new(18, "mulugeta");
+person = Person.new(18, 'mulugeta')
 puts person.id
 puts person.name
 puts person.age
--- student.rb
+++ student.rb
@@ -1,17 +1,17 @@
-require "./person.rb"
+require './person'
 
 class Student < Person
-  def initialize(classroom, age, name = "unknown", parent_permission = true)
+  def initialize(classroom, age, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '¯(ツ)/¯'
   end
 end
 
-person = Student.new("10th A", 18, "mulugeta", true);
+person = Student.new('10th A', 18, 'mulugeta', true)
 puts person.id
 puts person.name
 puts person.age
