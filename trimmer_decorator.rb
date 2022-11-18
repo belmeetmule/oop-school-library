67e2833e0d871a6e06a7094513d24c5e7c1c5965
mule <belmeetmule@gmail.com>
2022-11-18 14:49:57 +0300

    correcting index range as per reveiw comments

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,6 +3,6 @@ require './decorator'
 class TrimmerDecorator < Decorator
   def correct_name
     result = @nameable.correct_name
-    result.length > 10 ? result[0, 10] : result
+    result.length > 10 ? result[0, 9] : result
   end
 end
