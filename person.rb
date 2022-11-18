dafe41452d769b44a842bffcd8121e1d852c1639
mule <belmeetmule@gmail.com>
2022-11-19 01:46:51 +0300

    modify person class to implement m-2-m realtion with book

--- person.rb
+++ person.rb
@@ -7,8 +7,9 @@ class Person < Nameable
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals=[]
   end
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def of_age?
