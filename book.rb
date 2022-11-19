55b78932a9f5b2a03f109f9860ca6efc983eb95f
mule <belmeetmule@gmail.com>
2022-11-20 02:19:19 +0300

    update book class to add getter method

--- book.rb
+++ book.rb
@@ -1,10 +1,15 @@
 class Book
   def initialize(title, author)
+    @id = Random.new.rand(1..100)
     @title = title
     @author = author
     @rentals = []
   end
 
   attr_accessor :title, :author
-  attr_reader :rentals
+  attr_reader :rentals, :id
+
+  def add_rental(person, date)
+    Rental.new(person, date, self)
+  end
 end
