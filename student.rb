2bb6f06d63b49a3c70d40a66607c57ca49474864
mule <belmeetmule@gmail.com>
2022-11-20 02:20:52 +0300

    added type attribute

--- student.rb
+++ student.rb
@@ -1,12 +1,13 @@
 require './person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission)
+  def initialize(classroom, age, name: 'unknown', parent_permission: true, type: 'Student')
+    super(age, name: name, parent_permission: parent_permission)
     @classroom = classroom
+    @type = type
   end
 
-  attr_reader :classroom
+  attr_reader :classroom, :type
 
   def play_hooky
     '¯(ツ)/¯'
@@ -17,9 +18,3 @@ class Student < Person
     classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
-
-person = Student.new('10th A', 18, 'mulugeta', true)
-puts person.id
-puts person.name
-puts person.age
-puts person.play_hooky
